9f5671c44281e7368ee647bf3ef5610b0bf2d7e1
Puran Rijal <puranrijal246@gmail.com>
2026-07-03 22:17:34 +0545

    Config — add SQLAlchemy database URI

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ MYSQL_HOST = "localhost"
 MYSQL_USER = "root"
 MYSQL_PASSWORD = "root1234"
 MYSQL_DATABASE = "phishguard_db"
+
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root1234@localhost/phishguard_db"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
